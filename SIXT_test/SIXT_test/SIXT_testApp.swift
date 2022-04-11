dd3f9415a43c7c766b0a9df83672d5fdaca2fb12
Igor Kulik <phitochai@gmail.com>
2022-04-11 19:08:20 +0300

    Initial commit

---
+++ SIXT_test/SIXT_test/SIXT_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIXT_testApp.swift
+//  SIXT_test
+//
+//  Created by Igor Kulik on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SIXT_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
